0956a3562a4ef5656bccb97095ee03b3a205e332
IT24104176 <it24104176@my.sliit.lk>
2024-11-01 09:53:07 +0530

    Add files via upload

---
+++ Q4/IT24104176Lab3Q4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT24104176Lab3Q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a five-digit number: ");
+        int number = scanner.nextInt();
+
+        if (number >= 10000 && number <= 99999) {
+            String numberString = Integer.toString(number);
+            for (int i = 0; i < numberString.length(); i++) {
+                System.out.print(numberString.charAt(i) + " ");
+            }
+        } else {
+            System.out.println("Please enter a five-digit number:");
+        }
+    }
+}
